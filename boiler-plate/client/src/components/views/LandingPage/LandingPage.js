8ed23ffb1410152ae692bb0e95bf92519156beb0
rycando <rycando@naver.com>
2020-10-20 00:03:19 +0900

    finish

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { useEffect } from 'react'
+import Axios from 'axios'
+import { PromiseProvider } from 'mongoose'
+import { withRouter } from 'react-router-dom'
 
-function LandingPage() {
+function LandingPage(props) {
+
+    const onClickHandler = () => {
+        Axios.get('/api/users/logout')
+        .then(response => {
+            if(response.data.success) {
+                props.history.push("/login")
+            } else {
+                alert('로그아웃하는데 실패했습니다.')
+            }
+        })
+    }
+
     return (
-        <div>
-            LandingPage
+        <div style={{ display:'flex', justifyContent:'center', alignItems: 'center'
+        ,width: '100%', height: '100vh'}}>
+            <h2>시작 페이지</h2>
+
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
 
-export default LandingPage
+export default withRouter(LandingPage)
