f90c029d0effec7ec737d05638eedb145d569445
rycando <rycando@naver.com>
2020-11-03 21:39:12 +0900

    movie app + videotube

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -18,7 +18,7 @@ function LandingPage(props) {
     }
 
     return (
-        <div style={{ display:'flex', justifyContent:'center', alignItems: 'center'
+        <div style={{ display:'flex', flexFlow:'column', justifyContent:'center', alignItems: 'center'
         ,width: '100%', height: '100vh'}}>
             <h2>시작 페이지</h2>
 
